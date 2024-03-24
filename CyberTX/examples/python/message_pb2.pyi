a765703b7f165dbc25aa093fe1f2ba6164f6793b
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2024-03-23 23:24:20 -0700

    protoc version 3.20 and pyi out

---
+++ CyberTX/examples/python/message_pb2.pyi
@@ -0,0 +1,24 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar, Iterable, Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class PPMUpdate(_message.Message):
+    __slots__ = ["channel", "channel_value", "line"]
+    CHANNEL_FIELD_NUMBER: ClassVar[int]
+    CHANNEL_VALUE_FIELD_NUMBER: ClassVar[int]
+    LINE_FIELD_NUMBER: ClassVar[int]
+    channel: int
+    channel_value: int
+    line: int
+    def __init__(self, line: Optional[int] = ..., channel: Optional[int] = ..., channel_value: Optional[int] = ...) -> None: ...
+
+class PPMUpdateAll(_message.Message):
+    __slots__ = ["channel_values", "line"]
+    CHANNEL_VALUES_FIELD_NUMBER: ClassVar[int]
+    LINE_FIELD_NUMBER: ClassVar[int]
+    channel_values: _containers.RepeatedScalarFieldContainer[int]
+    line: int
+    def __init__(self, line: Optional[int] = ..., channel_values: Optional[Iterable[int]] = ...) -> None: ...
