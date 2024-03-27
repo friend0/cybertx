7109646bc162d83fafe2d0710e746aa1f5c115aa
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2024-03-26 20:38:57 -0700

    latest minimal python client example

--- CyberTX/examples/python/cybertx_server.py
+++ CyberTX/examples/python/cybertx_server.py
@@ -1,7 +1,15 @@
+"""
+Send a single PPM update packet.
+
+Expects that a CyberTX teensy is available on COM3.
+
+"""
+
 import serial
 import time
 from message_pb2 import PPMUpdate, PPMUpdateAll
 
+# TODO: cross-platform implementation
 arduino = serial.Serial('COM3', baudrate=115200, timeout=0)
 
 newMessage = PPMUpdate()
@@ -9,40 +17,12 @@ newMessage = PPMUpdate()
 newMessage.channel = 1
 newMessage.channel_value = 1500
 
-# arduino.write(newMessage.SerializeToString())
 ppm = PPMUpdateAll(line=1,
                    channel_values=[
-                       1500,
-                       1500,
-                       1500,
-                       1500,
-                       1500,
-                       1500,
-                       1500,
-                       1500,
-                       1500,
-                       1500,
-                       1500,
-                       1500,
-                       1500,
-                       1500,
-                       1500,
-                       1500,
+                       1700, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000,
+                       2000, 2000, 2000, 2000, 2000, 2000, 2000
                    ])
 ser_str = ppm.SerializeToString()
-# print(ser_str, ppm.ByteSize(), ser_str,
-#       PPMUpdateAll().FromString(ser_str).channel_values)
+print(ser_str, ppm.ByteSize(), ser_str,
+      PPMUpdateAll().FromString(ser_str).channel_values)
 arduino.write(ser_str)
-
-time.sleep(.5)
-
-# num = arduino.read()
-# cv = arduino.read()
-# while(arduino.)
-# res = arduino.read(18)
-# print(f"num {int.from_bytes(num)}")
-# print(f"cv {int.from_bytes(cv)}")
-# print(res)
-# ppm = PPMUpdateAll()
-# print(ppm.ParseFromString(res))
-# print(ppm.channel_values)
