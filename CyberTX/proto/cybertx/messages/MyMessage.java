3c110c20f9ec5ce49a77ebbb2f7b60913b0f8457
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2024-04-27 19:52:54 -0700

    remove old cybertx messages dir

--- CyberTX/proto/cybertx/messages/MyMessage.java
+++
@@ -1,9 +0,0 @@
-package cybertx.messages;
-
-import cybertx.messages.Message;
-
-public class MyMessage {
-   public static Message.PPMUpdate.Builder newBuilder() {
-      return Message.PPMUpdate.newBuilder();
-   }
-}
