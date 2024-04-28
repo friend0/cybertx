259ca5d1a60a06bd7fbc957381f82429370fea05
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2024-04-27 19:46:20 -0700

    updated message to ppm

---
+++ CyberTX/proto/cybertx/messages/MyMessage.java
@@ -0,0 +1,9 @@
+package cybertx.messages;
+
+import cybertx.messages.Message;
+
+public class MyMessage {
+   public static Message.PPMUpdate.Builder newBuilder() {
+      return Message.PPMUpdate.newBuilder();
+   }
+}
