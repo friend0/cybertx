9074abb5900985c30b3469cbf1aa4dc5ad2795e3
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2024-10-17 21:50:45 -0700

    test gui

--- CyberTX/clients/MATLAB/test.m
+++ CyberTX/clients/MATLAB/test.m
@@ -1,5 +1,6 @@
-numChannels = 16;
+numChannels = 6;
 ctx = CyberTX("COM3", 38400, numChannels);
+ctx
 ppmValues = zeros(1,numChannels,'uint16');
 for i=1:numChannels
     ppmValues(i) = 1000 + 1000/16 * (i-1);
